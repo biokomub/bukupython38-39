0ce4cbd211658021e78279ec37b6a3e9b2c07054
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 06:23:51 +0700

    Create kode56.py
    Contoh membuat list, tuple, set, frozenset

---
+++ Bab 5/kode56.py
@@ -0,0 +1,13 @@
+#contoh membuat list
+lDNAbases = ['Cytosine', 'Thymine', 'Guanine', 'Adenine']
+
+#contoh membuat tuple
+tDNAbases = ('Cytosine', 'Thymine', 'Guanine', 'Adenine')
+
+#contoh membuat set
+DNAbases = ['Cytosine', 'Thymine', 'Guanine', 'Adenine']
+sDNAbases = set(DNAbases)
+
+#contoh membuat frozenset
+DNAbases = ['Cytosine', 'Thymine', 'Guanine', 'Adenine']
+fzsDNAbases = frozenset(DNAbases)
