f108bd4ee819c312a69dcf1cbf544d70edd7a0f6
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 06:20:56 +0700

    Create kode53.py
    Contoh iterasi for dengan iteran akhir, tanpa awal dan step

---
+++ Bab 5/kode53.py
@@ -0,0 +1,9 @@
+#contoh iterasi for dengan iteran akhir, tanpa awal 
+#dan step
+
+init = int(input('Input nilai inisial: '))
+iter = int(input('Input banyak iterasi: '))
+step = int(input('Input nilai step kenaikan: '))
+
+for i in range(iter): 
+    print(i)
