46dd4e605b0b696e4bc2c8c87fc176ead108c056
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-09 23:59:45 +0700

    Create kode35.py
    Contoh Indentasi pada Java

---
+++ Bab 3/kode35.py
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main (String [] args ){
+        int nilai = 100;
+        if (nilai > 90){
+            System.out.println("Lebih dari 90");
+        }     
+        else{
+            System.out.println("Kurang dari 90");
+        }
+    }
+}
