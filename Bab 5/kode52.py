d311453a9a2e83164e6fcb76fcb1730340593dcc
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 06:19:53 +0700

    Create kode52.py
    Contoh iterasi for dengan iteran min dan max, tanpa step

---
+++ Bab 5/kode52.py
@@ -0,0 +1,7 @@
+#contoh iterasi for dengan iteran min dan max, tanpa step
+init = int(input('Input nilai inisial: '))
+iter = int(input('Input banyak iterasi: '))
+step = int(input('Input nilai step kenaikan: '))
+
+for i in range(init,iter): 
+    print(i)
