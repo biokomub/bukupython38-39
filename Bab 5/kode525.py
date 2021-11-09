ad3379386dbab7a7ff371ce5c39febb1dfd0b6af
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 06:53:09 +0700

    Create kode525.py
    Contoh penggunaan while dan continue

---
+++ Bab 5/kode525.py
@@ -0,0 +1,13 @@
+#contoh penggunaan while dan continue
+
+i = 0
+
+print('Ini sebelum while')
+
+while (i <= 5):
+    continue #continue sebelum print
+    print(i)
+    print('Ini di dalam while')
+    i += 1
+
+print('Ini sesudah while')
