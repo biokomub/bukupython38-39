c063754008169d16564e70e67131850f0d0dc0b2
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 06:50:29 +0700

    Create kode521.py
    Contoh for tanpa continue

---
+++ Bab 5/kode521.py
@@ -0,0 +1,11 @@
+#contoh iterasi for dengan penggunaan continue
+
+#contoh for tanpa continue
+print("Ini sebelum for")
+
+for i in range(0,6,1): 
+    continue
+    print(i)
+    print("Ini didalam for")
+
+print("Ini sesudah for")
