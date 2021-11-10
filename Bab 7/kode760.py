0feefeba263dcb6495a7971966b0160e58e62e9e
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 10:07:54 +0700

    Update kode760.py
    contoh mencetak string berpola dengan template string

--- Bab 7/kode760.py
+++ Bab 7/kode760.py
@@ -1,1 +1,14 @@
-aa
+#contoh mencetak string berpola dengan template string
+from string import Template
+
+dict = {"nama" : "Maulana", "prodi" : "Biologi",
+        "univ_asal" : "Universitas Brawijaya",
+        "akt" : 2012, "profesi" : "Mahasiswa Doktoral",
+        "tempat" : "KMUTT Thailand"}
+
+t = Template("$nama adalah alumni $prodi $univ_asal \
+angkatan $akt yang kini berprofesi sebagai $profesi \
+di $tempat.")
+ts = t.safe_substitute(**dict)
+
+print(ts)
