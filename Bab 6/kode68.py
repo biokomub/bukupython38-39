4c9b5387bc6f2251accd95844456139a69cce071
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 07:20:31 +0700

    Update kode68.py
    Contoh try... except... else...

--- Bab 6/kode68.py
+++ Bab 6/kode68.py
@@ -1,1 +1,11 @@
-aa
+#contoh try... except... else... 
+
+try:
+    a = input("Masukkan angka pertama: ")
+    b = input("Masukkan angka kedua: ")
+    hslbg = int(a)/int(b)
+    print("Hasil baginya: ", hslbg) 
+except (ValueError, ZeroDivisionError):
+    print("Ada yang salah...")
+else:
+    print("Pangkat dua hasil baginya: ", hslbg**2)
