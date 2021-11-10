c2923bd9175fd752aa0f983f13040b308fb3b80b
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 07:25:36 +0700

    Update kode612.py
    Contoh penangkapan kesalahan multi eksepsi

--- Bab 6/kode612.py
+++ Bab 6/kode612.py
@@ -1,1 +1,17 @@
-a
+#contoh penangkapan semua kesalahan
+
+try:
+    a = input("Masukkan angka pertama: ")
+    b = input("Masukkan angka kedua: ")
+    hslbg = int(a)/int(b)
+    print("Hasil baginya: ", hslbg) 
+except(ZeroDivisionError):
+    print("Tidak boleh dibagi 0")
+except (ValueError):
+    print("Value Error, harus angka yang dimasukkan")
+except (KeyboardInterrupt):
+    print("Program dihentikan lewat keyboard")
+else:
+    print("Pangkat dua hasil baginya: ", hslbg**2)
+finally:
+    print("Akhirnya, selesai.")
