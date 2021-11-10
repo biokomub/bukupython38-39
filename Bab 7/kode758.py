33365dc868cf1c528c06a3eb6274641acd2fc920
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 10:09:03 +0700

    Update kode758.py
    contoh mencetak string berpola dengan template string

--- Bab 7/kode758.py
+++ Bab 7/kode758.py
@@ -1,1 +1,13 @@
-aa
+#contoh mencetak string berpola dengan template string
+from string import Template
+
+dict = {"nama" : "Maulana", "prodi" : "Biologi",
+        "univ_asal" : "Universitas Brawijaya",
+        "akt" : 2012, "profesi" : "Mahasiswa Doktoral"}
+
+t = Template("$nama adalah alumni $prodi $univ_asal \
+angkatan $akt yang kini berprofesi $profesi sebagai \
+di $tempat.")
+ts = t.substitute(**dict)
+
+print(ts)
