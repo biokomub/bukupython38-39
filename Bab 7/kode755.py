32cdb7a1cdf33c329f89fb977fd5da6f345160fc
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 10:12:37 +0700

    Update kode755.py
    contoh mencetak string berpola dengan template string

--- Bab 7/kode755.py
+++ Bab 7/kode755.py
@@ -1,1 +1,7 @@
-a
+#contoh mencetak string berpola dengan template string
+from string import Template
+
+t = Template("Hallo, saya $nama dari angkatan $akt!")
+ts = t.substitute(nama="Bayu", akt=2014)
+
+print(ts)
