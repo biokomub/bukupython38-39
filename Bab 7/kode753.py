f416446ba96863cf306bc7920605b753445cdbfd
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 10:14:06 +0700

    Update kode753.py
    contoh mencetak string berpola dengan template string

--- Bab 7/kode753.py
+++ Bab 7/kode753.py
@@ -1,1 +1,8 @@
-a
+#contoh mencetak string berpola dengan template string
+from string import Template
+a = "Arfan"
+
+t = Template("Hallo semuanya, saya $nama!")
+ts = t.substitute(nama=a)
+
+print(ts)
