1ed4695b826f35700e1e8671b5609a390475954e
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 07:28:08 +0700

    Update kode615.py
    Conoth penggunaan traceback

--- Bab 6/kode615.py
+++ Bab 6/kode615.py
@@ -1,1 +1,14 @@
-a
+#contoh penggunaan traceback
+
+import traceback 
+
+num = "5"
+numB = None
+
+try:
+    numB = (num+1) * 2
+except BaseException as e:
+    e = traceback.format_exc()
+    print('Error: ', e)
+
+print(numB)
