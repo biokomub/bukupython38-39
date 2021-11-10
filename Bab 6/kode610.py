69850b6892f1459996aa43b52c887842bafb3f48
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 07:22:16 +0700

    Update kode610.py
    Contoh try... finally...

--- Bab 6/kode610.py
+++ Bab 6/kode610.py
@@ -1,1 +1,9 @@
-aa
+#contoh try... finally...
+
+try:
+    a = input("Masukkan angka pertama: ")
+    b = input("Masukkan angka kedua: ")
+    hslbg = int(a)/int(b)
+    print("Hasil baginya: ", hslbg) 
+finally:
+    print("Akhirnya, selesai.")
