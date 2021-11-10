e4e335a5880288388f362c64cf02d20243ca8cf0
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 07:21:26 +0700

    Update kode69.py
    Contoh try... except... finally...

--- Bab 6/kode69.py
+++ Bab 6/kode69.py
@@ -1,1 +1,11 @@
-aa
+#contoh try... except... finally...
+
+try:
+    a = input("Masukkan angka pertama: ")
+    b = input("Masukkan angka kedua: ")
+    hslbg = int(a)/int(b)
+    print("Hasil baginya: ", hslbg) 
+except (ValueError, ZeroDivisionError):
+    print("Ada yang salah...")
+finally:
+    print("Akhirnya, selesai.")
