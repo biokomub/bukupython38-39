3f103b5ae63d68ec315ba45ab5b8fdab647be361
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 10:15:25 +0700

    Update kode751.py
    contoh penggunaan :#parameter

--- Bab 7/kode751.py
+++ Bab 7/kode751.py
@@ -1,1 +1,9 @@
-a
+#contoh penggunaan :#parameter
+a = 42
+
+print(f"Isi dari variabel a adalah : {a}")     
+print(f"Integernya : {a:#d}")
+print(f"Floatnya : {a:#f}")
+print(f"Binernya : {a:#b}")
+print(f"Octalnya : {a:#o}")
+print(f"Hexnya : {a:#x}")
