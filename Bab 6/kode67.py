79e706c4154da9154bcf77f5118a3960256f5088
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 07:19:39 +0700

    Update kode67.py
    Contoh try... except...

--- Bab 6/kode67.py
+++ Bab 6/kode67.py
@@ -1,1 +1,9 @@
-aa
+#contoh try... except...
+
+try:
+    a = input("Masukkan angka pertama: ")
+    b = input("Masukkan angka kedua: ")
+    hslbg = int(a)/int(b)
+    print("Hasil baginya: ", hslbg) 
+except (ValueError, ZeroDivisionError):
+    print("Ada yang salah...")
