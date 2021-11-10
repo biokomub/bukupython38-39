1c2a4a0a5efe32f3e1bc5295223f761607bab4e3
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2021-11-10 10:25:54 +0700

    Update kode746.py
    contoh mencetak kurung kurawal dengan f-string yang benar

--- Bab 7/kode746.py
+++ Bab 7/kode746.py
@@ -1,1 +1,18 @@
-aa
+#contoh mencetak kurung kurawal dengan f-string yang benar
+a = f"ini { 1 + 1 }"
+b = f"ini {{ 1 + 1 }}"
+c = f"ini {{{ 1 + 1 }}}"
+d = f"ini {{{{ 1 + 1 }}}}"
+e = f"ini {{{{{ 1 + 1 }}}}}"
+f = f"ini {{{{{{ 1 + 1 }}}}}}"
+g = f"ini {{{{{{{ 1 + 1 }}}}}}}"
+h = f"ini {{{{{{{{ 1 + 1 }}}}}}}}"
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
