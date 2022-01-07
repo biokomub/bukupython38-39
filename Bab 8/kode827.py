a256126acb0397dc721de323c4b440ad9f5c6225
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 18:28:57 +0700

    Create kode827.py

---
+++ Bab 8/kode827.py
@@ -0,0 +1,9 @@
+#membuat objek array bertipe integer
+tuple1 = (0, 50, 100, 200)
+print(tuple1)
+
+#menghitung frekuensi elemen tertentu dalam tuple
+print(tuple1.count(0))
+
+#mengetahui posisi indeks elemen tertentu dalam tuple
+print(tuple1.index(0))
