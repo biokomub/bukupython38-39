1d510ac3459e7eb2b9bc65c6dc3382d23aa6a8a5
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 20:43:41 +0700

    Create kode1154.py

---
+++ Bab 11/kode1154.py
@@ -0,0 +1,21 @@
+from plum import dispatch
+
+class Contoh(object):
+      
+    @dispatch
+    def tambah(self, x: int, y: int):
+        return (x + y)
+  
+    @dispatch
+    def tambah(self, x: str, y: str):
+        return (x + y)
+    
+    @dispatch
+    def tambah(self, x: complex, y: complex):
+        return ("Tidak menerima kompleks")
+    
+#pembuat objek dan caller  
+obj = Contoh()
+print("Hasil:", obj.tambah(3, 7))
+print("Hasil:", obj.tambah("Haha", "Hihi"))
+print("Hasil:", obj.tambah(6j, 4j))
