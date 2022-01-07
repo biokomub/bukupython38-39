e3831637373ef476f4eadb5a0fadac97cef60612
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 22:02:09 +0700

    Update kode728.py

--- Bab 7/kode728.py
+++ Bab 7/kode728.py
@@ -1,1 +1,14 @@
-aa
+# angka
+a = 123
+
+# menampilkan integer
+print("Angkanya: {:d}".format(a))
+
+# menampilkan float
+print("Bentuk floatnya: {:f}".format(a))
+
+# menampilkan biner, octal, dan hex
+print("Bentuk binernya: {:b}".format(a))
+print("Bentuk octalnya : {:o}".format(a))
+print("Bentuk hexnya: {:x}".format(a))
+print("Bentuk karakternya: {:c}".format(a))
