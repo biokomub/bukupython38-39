6bfc04cb9d3ca6b489b6aa238932ba301b937beb
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 21:58:55 +0700

    Update kode723.py

--- Bab 7/kode723.py
+++ Bab 7/kode723.py
@@ -1,1 +1,17 @@
-a
+#mencetak string dengan %-formatting banyak parameter
+
+#banyak parameter satu tipe
+a1 = 24 
+a2 = 35
+a3 = 36
+
+print("Isi A1, A2, dan A3 adalah: %i, %i, %i." % 
+(a1, a2, a3))
+
+#banyak parameter banyak tipe
+b1 = 24 
+b2 = 35
+b3 = 36
+
+print("Isi B1, B2, dan B3 adalah: %i, %s, %f." % 
+(a1, a2, a3))
