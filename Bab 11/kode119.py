8ed80605f8c86116161c2a33b9d053fd2a0da6d2
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 20:22:56 +0700

    Create kode119.py

---
+++ Bab 11/kode119.py
@@ -0,0 +1,9 @@
+class Tumbuhan(object): 
+    def __init__(self, nama, nama_latin):  
+        print("Tumbuhan", nama, 
+              "nama ilmiahnya", nama_latin + ".") 
+
+tumb1 = Tumbuhan("Rumput Teki", "Cyperus rotundus")
+tumb2 = Tumbuhan("Tebu", "Saccharum officinarum")
+tumb3 = Tumbuhan("Padi", "Oryza sativa")
+tumb4 = Tumbuhan("Jagung", "Zea mays")
