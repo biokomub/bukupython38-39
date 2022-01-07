82c87cc010b02a52942f8981f7f7b5e3a201aa5b
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 18:37:07 +0700

    Create kode846.py

---
+++ Bab 8/kode846.py
@@ -0,0 +1,9 @@
+#membuat set dan frozenset
+contohset = set({11, 22, 33, 33, 44, 44, 55, 66, 77, 77})
+contohfrzset = frozenset({11, 22, 22, 44, 55, 55, 88, 99})
+
+#mencetak isi set
+print("Isi set: " + str(contohset))
+
+#mencetak isi set
+print("Isi frozenset: " + str(contohfrzset))
