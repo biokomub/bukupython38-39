93835856aecd5d593d7576d608087437b3ae1586
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 20:21:40 +0700

    Create kode118.py

---
+++ Bab 11/kode118.py
@@ -0,0 +1,10 @@
+class Tumbuhan(str): 
+    def __new__(cls, nama, nama_latin):  
+        print("Tumbuhan", nama, 
+              "nama ilmiahnya", nama_latin + ".") 
+        return str.__new__(cls, (nama, nama_latin))  
+
+tumb1 = Tumbuhan("Rumput Teki", "Cyperus rotundus")
+tumb2 = Tumbuhan("Tebu", "Saccharum officinarum")
+tumb3 = Tumbuhan("Padi", "Oryza sativa")
+tumb4 = Tumbuhan("Jagung", "Zea mays")
