05579a90952781d27898c35763820a777afa6b11
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 22:04:59 +0700

    Update kode734.py

--- Bab 7/kode734.py
+++ Bab 7/kode734.py
@@ -1,1 +1,20 @@
-a
+# string padding dengan rata kiri
+print("{:<11d}".format(32768))
+
+# string padding dengan rata kiri
+# dan diapitkan dengan karakter "*"
+print("{:*<11d}".format(32768))
+
+# string padding dengan rata kanan
+print("{:>11d}".format(32768))
+
+# string padding dengan rata kanan
+# dan diapitkan dengan karakter "*"
+print("{:*>11d}".format(32768))
+
+# string padding dengan rata tengah
+print("{:^11d}".format(32768))
+
+# string padding dengan rata tengah
+# dan diapitkan dengan karakter "*"
+print("{:*^11d}".format(32768))
