6156e00012ffb14b3ad04ec3bb2ee4063dd8635e
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 20:25:08 +0700

    Create kode1113.py

---
+++ Bab 11/kode1113.py
@@ -0,0 +1,10 @@
+class kelasA(object):
+    def __new__(cls):
+        print("Buat instance dari kelas kelasA")
+        return super(kelasA, cls).__new__(cls)
+  
+    def __init__(self):
+        print("Init kelas kelasA dipanggil")
+        return "kembali"
+        
+kelasA()
