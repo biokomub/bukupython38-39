69072b6190f4c1e028e966b1e339a0613a1fcca4
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 22:03:08 +0700

    Update kode730.py

--- Bab 7/kode730.py
+++ Bab 7/kode730.py
@@ -1,1 +1,9 @@
-aa
+# padding akan kehilangan fungsi bila menerima 
+# ukuran angka lebih besar dari padding yang disediakan 
+print("{:5d}".format(1234))
+print("{:5d}".format(12345))
+print("{:5d}".format(123456))
+
+# hal yang sama terjadi juga di float  
+print("{:5f}".format(12345))
+print("{:5f}".format(123456))
