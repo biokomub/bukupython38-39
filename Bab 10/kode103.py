3337d6233a4363438d76c26c64c18f2518d7e4c7
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 19:33:53 +0700

    Create kode103.py

---
+++ Bab 10/kode103.py
@@ -0,0 +1,9 @@
+#contoh penulisan fungsi dengan pengembalian nilai
+
+def perkalian(a, b): 
+    hasil = a * b
+    return hasil
+
+#mengeluarkan nilai yang dikembalikan fungsi
+ret = perkalian(5, 10)
+print(ret)
