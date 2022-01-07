f2589aae3e264e19c8d54ca9acc1f9d4beef31f7
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 20:26:11 +0700

    Create kode1115.py

---
+++ Bab 11/kode1115.py
@@ -0,0 +1,31 @@
+class SA(object):
+    nama = "" #ini variabel kelas
+    tipe = ""
+    jabatan = "" 
+
+    def __init__(self, nama, tipe, jabatan): #konstruktor
+        print("Konstruktor dipanggil\n")     
+        self.nama = nama
+        self.tipe = tipe
+        self.jabatan = jabatan
+ 
+    def __del__(self):
+        print("Destruktor dipanggil")
+        print("Data dibersihkan")
+        del self.nama
+        del self.tipe
+        del self.jabatan
+
+    def tampilData(self): #metode untuk menampilkan data
+        print("Tampil Data")
+        print("Nama    : ", self.nama)
+        print("Tipe    : ", self.tipe)
+        print("Jabatan : ", self.jabatan)
+        print("")
+
+
+org3 = SA("Dr. Brian Rahardi",
+          "Dosen",
+          "Dosen Biologi UB")
+
+org3.tampilData()
