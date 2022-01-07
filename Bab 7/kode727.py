4ff9ffeb89fc02b8d1784c2da8174cd03f110110
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 22:01:42 +0700

    Update kode727.py

--- Bab 7/kode727.py
+++ Bab 7/kode727.py
@@ -1,1 +1,26 @@
-a
+#nama asli variabel
+a = "Benny"
+b = 42
+
+#cetak dengan mode default
+print("Nama saya {}, umur saya {} tahun.".format(
+    a, b)
+    )
+
+#cetak dengan positional-based param/args
+#(indeks ditulis eksplisit)
+print("Nama saya {0}, umur saya {1} tahun.".format(
+    a, b)
+    )
+
+#cetak dengan keyword-based params/args
+#(menggunakan kunci) 
+print("Nama saya {nama}, umur saya {umur} tahun.".format(
+    nama = a, umur = b)
+    )
+
+#cetak dengan mode argumen campuran
+#(mixed arguments)
+print("Nama saya {0}, umur saya {umur} tahun.".format(
+    a, umur = b)
+    )
