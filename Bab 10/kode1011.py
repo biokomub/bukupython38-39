712127b2fba50167541e2e912740187bd73035d8
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 19:43:48 +0700

    Update kode1011.py

--- Bab 10/kode1011.py
+++ Bab 10/kode1011.py
@@ -1,14 +1,4 @@
-# variable scope
+def kali(a, b):
+    hasil = a * b
+    return hasil
 
-# ini variabel global
-var = 69
-
-#definisikan fungsi
-def fungsiScope():
-    global var
-    var = 96
-    return var
-    
-print("Isi variabel di luar fungsi: ", var)
-print("Isi variabel di dalam fungsi: ", fungsiScope())
-print("Isi variabel di luar fungsi sekarang: ", var)
