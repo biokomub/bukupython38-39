895bf0f5187f87dc04d4caf3fac40780f80dcde2
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 18:18:34 +0700

    Create kode87.py

---
+++ Bab 8/kode87.py
@@ -0,0 +1,7 @@
+#mengekstrak elemen tuple dengan indeks
+item_dagang = ["apel", 10, 5000, "Rp 50.000,00"]
+
+print(item_dagang[0])
+print(item_dagang[1])
+print(item_dagang[2])
+print(item_dagang[3])
