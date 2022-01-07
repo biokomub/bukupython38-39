f466c500206725974594a39ba17233d90877a95e
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 18:36:05 +0700

    Create kode844.py

---
+++ Bab 8/kode844.py
@@ -0,0 +1,10 @@
+#membuat set
+contohset = set({11, 22, 33, 44, 55})
+set2list = list(contohset)
+
+#mengekstrak elemen set dari hasil konversi set -> list
+print(set2list[0])
+print(set2list[1])
+print(set2list[2])
+print(set2list[3])
+print(set2list[4])
