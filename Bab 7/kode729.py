930f4048d41a3aa44615f62beb5bf481aba1a67a
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 22:02:41 +0700

    Update kode729.py

--- Bab 7/kode729.py
+++ Bab 7/kode729.py
@@ -1,1 +1,16 @@
-aa
+# mengatur padding pada string 
+print("{:5}".format("Angka"))
+
+# mengatur padding pada integer
+print("{:5d}".format(1))
+print("{:5d}".format(12))
+print("{:5d}".format(123))
+print("{:5d}".format(1234))
+print("{:5d}".format(12345))
+
+# mengatur padding pada float
+print("{:5f}".format(1))
+print("{:5f}".format(12))
+print("{:5f}".format(123))
+print("{:5f}".format(1234))
+print("{:5f}".format(12345))
