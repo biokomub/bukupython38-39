dfc1489aca0260418ffbdf45c9a0942b84874dba
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 18:20:16 +0700

    Create kode810.py

---
+++ Bab 8/kode810.py
@@ -0,0 +1,6 @@
+#mengetahui panjang list
+judul_tabel = ["item", "jumlah", "harga/item", "total harga"]
+item_dagang = ["apel", 10, 5000, "Rp 50.000,00"]
+
+print(len(judul_tabel))
+print(len(item_dagang))
