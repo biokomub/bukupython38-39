745f6f560f634d3a65075b2b8b95357ea1d0f5da
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 20:16:02 +0700

    Create kode1059.py

---
+++ Bab 10/kode1059.py
@@ -0,0 +1,24 @@
+def generator(nilai_awal, jumlah_isi, step):
+
+    nilai = nilai_awal
+    while (((nilai_awal != None) or 
+            (jumlah != None)) == True):
+        nilai_isian = (yield nilai)
+        if nilai_isian is None:
+            nilai = nilai + step
+        else:
+            nilai = nilai_isian
+    
+    yield nilai
+
+#program_utama
+jml_isian = 10
+obj_gen = generator(0, jml_isian, 1)
+
+batas = jml_isian   
+for i in range(batas):   
+    print(i, obj_gen.__next__())
+    if ((i == 0) == True):
+        obj_gen.send(20)
+        
+obj_gen.close()
