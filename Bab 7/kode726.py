6b926047cd310f965b69122045535c89a545c14f
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 22:00:56 +0700

    Update kode726.py

--- Bab 7/kode726.py
+++ Bab 7/kode726.py
@@ -1,1 +1,10 @@
-a
+#bekerja dengan variabel substitusi di str.format()
+
+#nama asli variabel
+a = "Andri"
+b = 28
+
+#cetak dengan variabel substitusi
+print("Nama saya {nama}, umur saya {umur} tahun.".format(
+    nama = a, umur = b)
+    )
