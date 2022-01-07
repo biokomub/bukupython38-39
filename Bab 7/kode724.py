587358761967ca611a97cc196294bf7872f96446
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 21:59:53 +0700

    Update kode724.py

--- Bab 7/kode724.py
+++ Bab 7/kode724.py
@@ -1,1 +1,10 @@
-a
+#bekerja dengan variabel substitusi di %-formatting 
+
+#nama asli variabel
+a = "Andri"
+b = 28
+
+#cetak dengan variabel substitusi
+print("Nama saya %(nama)s, umur saya %(umur)i tahun." % \
+    {"nama" : a, "umur" : b} \
+     )
