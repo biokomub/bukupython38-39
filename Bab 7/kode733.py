d5fd7f5127e52acc95b62a003e2c7d8a1f88900d
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 22:04:35 +0700

    Update kode733.py

--- Bab 7/kode733.py
+++ Bab 7/kode733.py
@@ -1,1 +1,20 @@
-a
+# string padding dengan rata kiri
+print("{:<11}".format("Biologi"))
+
+# string padding dengan rata kiri
+# dan diapitkan dengan karakter "*"
+print("{:*<11}".format("Biologi"))
+
+# string padding dengan rata kanan
+print("{:>11}".format("Biologi"))
+
+# string padding dengan rata kanan
+# dan diapitkan dengan karakter "*"
+print("{:*>11}".format("Biologi"))
+
+# string padding dengan rata tengah
+print("{:^11}".format("Biologi"))
+
+# string padding dengan rata tengah
+# dan diapitkan dengan karakter "*"
+print("{:*^11}".format("Biologi"))
