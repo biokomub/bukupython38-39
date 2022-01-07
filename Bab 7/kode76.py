d2542dc4d9c4703780ad5023ece478a6acf39256
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 21:48:39 +0700

    Update kode76.py

--- Bab 7/kode76.py
+++ Bab 7/kode76.py
@@ -1,1 +1,17 @@
-aa
+#contoh string indexing
+
+string = "Biologi"
+
+A = string[0]
+B = string[1]
+C = string[3]
+D = string[5]
+E = string[-1]
+F = string[-7]
+
+print("Indeks ke-0 adalah : " + A)
+print("Indeks ke-1 adalah : " + B)
+print("Indeks ke-3 adalah : " + C)
+print("Indeks ke-5 adalah : " + D)
+print("Indeks ke-(-1) adalah : " + D)
+print("Indeks ke-(-7) adalah : " + D)
