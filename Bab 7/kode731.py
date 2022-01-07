e3a98fc301edcfa63ac32255af640f28f599004e
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 22:03:44 +0700

    Update kode731.py

--- Bab 7/kode731.py
+++ Bab 7/kode731.py
@@ -1,1 +1,12 @@
-a
+# mengatur padding pada float dengan desimal yang diatur
+# desimal diatur presisinya,
+# dari presisi default (6) hingga kurang presisi (0) 
+# Catatan: (.f) equivalen dengan (.6f)
+print("{:5f}".format(12345))
+print("{:5.6f}".format(12345))
+print("{:5.5f}".format(12345))
+print("{:5.4f}".format(12345))
+print("{:5.3f}".format(12345))
+print("{:5.2f}".format(12345))
+print("{:5.1f}".format(12345))
+print("{:5.0f}".format(12345))
