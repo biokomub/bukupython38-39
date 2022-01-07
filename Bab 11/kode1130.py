7912773f2185975db7732f133353d7c460a25fc1
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 20:33:05 +0700

    Create kode1130.py

---
+++ Bab 11/kode1130.py
@@ -0,0 +1,93 @@
+class Padi(object):
+    def __init__(self, tinggi:int, umur_panen:int,
+                 pg:float, lg:float, tg:float,
+                 pb:float, lb:float, tb:float,
+                 ka:float):
+        self.tinggi = tinggi  
+        self.umur_panen = umur_panen
+        self.pg = pg 
+        self.lg = lg
+        self.tg = tg
+        self.pb = pb
+        self.lb = lb
+        self.tb = tb
+        self.ka = ka 
+
+    def cetakData(self):
+        print("Tinggi:", self.tinggi, "cm")
+        print("Umur Panen:", self.umur_panen, "hari")
+        print("Dimensi gabah:", self.pg, "x",
+              self.lg, "x",
+              self.tg, "mm")
+        print("Dimensi beras:", self.pb, "x",
+              self.lb, "x",
+              self.tb, "mm")
+        print("Kadar Amilosa:", self.ka)
+    
+    def klasifBeras(self):
+        if ((float(self.pb)) >= 7.0):
+            print("Ini beras tipe Bulir Panjang Kelas 1")
+        elif (((float(self.pb)) >= 6.6) and \
+              ((float(self.pb)) <= 7.0)):
+            print("Ini beras tipe Bulir Panjang Kelas 2")
+        elif (((float(self.pb)) >= 6.2) and \
+              ((float(self.pb)) <= 6.6)):
+            print("Ini beras tipe Bulir Panjang Kelas 3")
+        elif ((float(self.pb)) <= 6.2):
+            print("Ini beras tipe Bulir Pendek")
+        else:
+            print("Nilai panjang beras tidak valid")
+            
+    def klasifAmilosa(self):
+        if ((float(self.ka)) < 13.0):
+            print("Ini beras beramilosa rendah")
+        elif (((float(self.ka)) >= 13.0) and \
+              ((float(self.ka)) <= 20.0)):
+            print("Ini beras beramilosa sedang")
+        elif ((float(self.ka)) > 20.0):
+            print("Ini beras beramilosa tinggi")
+        else:
+            print("Nilai amilosa beras tidak valid")
+        
+class PadiThailand(Padi):
+    def __init__(self, nama_varietas, tinggi:int, 
+                 umur_panen:int, pg:float, lg:float, 
+                 tg:float, pb:float, lb:float, tb:float,
+                 ka:float, prod:float,
+                 info_ketahanan):
+        self.nama_varietas = nama_varietas #perluasan
+        self.tinggi = tinggi  
+        self.umur_panen = umur_panen
+        self.pg = pg 
+        self.lg = lg
+        self.tg = tg
+        self.pb = pb
+        self.lb = lb
+        self.tb = tb
+        self.ka = ka 
+        self.prod = prod #perluasan
+        self.info_ketahanan = info_ketahanan #perluasan
+        
+    def cetakData(self):
+        print("Nama Varietas Padi:", self.nama_varietas)
+        print("Tinggi:", self.tinggi, "cm")
+        print("Umur Panen:", self.umur_panen, "hari")
+        print("Dimensi gabah:", self.pg, "x",
+              self.lg, "x",
+              self.tg, "mm")
+        print("Dimensi beras:", self.pb, "x",
+              self.lb, "x",
+              self.tb, "mm")
+        print("Kadar Amilosa:", self.ka)
+        print("Produktivitas:", self.prod, "ton/ha")
+        print("Ketahanan:", self.info_ketahanan)
+        
+#buat objek padi
+objPadi = PadiThailand("RD41", "104", "105",
+                       "10.40", "2.5", "2.0",
+                       "7.7", "2.2", "1.8",
+                       "27.16", "4.5", "Wereng Coklat")
+
+objPadi.cetakData()
+objPadi.klasifBeras()
+objPadi.klasifAmilosa()
