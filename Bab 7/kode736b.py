8e2f5bad8242a355fe9688797f6d838569aeecd4
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 22:07:30 +0700

    Create kode736b.py

---
+++ Bab 7/kode736b.py
@@ -0,0 +1,16 @@
+#mendefinisikan dictionary
+record_profil = {"nama": "Maulana", "umur": 27, 
+                 "asal": "Malang", "pekerjaan": "Penulis",
+                 "asal_jurusan": "Biologi", 
+                 "asal_pt": " Universitas Brawijaya", 
+                 "angkatan": 2012}
+
+print("Halo, Semuanya! Perkenalkan, \
+nama saya {r[nama]}. \
+Umur saya {r[umur]} tahun. \
+Saya berasal dari {r[asal]}. \
+Pekerjaan saya adalah {r[pekerjaan]}. \
+Dulu saya berkuliah di {r[asal_jurusan]}{r[asal_pt]} \
+angkatan {r[angkatan]}. \
+Mohon kerjasamanya, terima kasih.".format(
+  r=record_profil))
