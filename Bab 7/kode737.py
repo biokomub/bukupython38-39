f938c94e2294bc033d047e982268c53ea72025a1
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 22:07:54 +0700

    Update kode737.py

--- Bab 7/kode737.py
+++ Bab 7/kode737.py
@@ -1,1 +1,19 @@
-aa
+#mendefinisikan kelas
+class record_profil:
+    nama = "Maulana"
+    umur = 27 
+    asal = "Malang"
+    pekerjaan = "Penulis"
+    asal_jurusan = "Biologi" 
+    asal_pt = " Universitas Brawijaya" 
+    angkatan = 2012
+
+print("Halo, Semuanya! Perkenalkan, \
+nama saya {r.nama}. \
+Umur saya {r.umur} tahun. \
+Saya berasal dari {r.asal}. \
+Pekerjaan saya adalah {r.pekerjaan}. \
+Dulu saya berkuliah di {r.asal_jurusan}{r.asal_pt} \
+angkatan {r.angkatan}. \
+Mohon kerjasamanya, terima kasih.".format(
+  r=record_profil))
