5ea1f9079cf45aaecd19d7322aa9c05871f48aa6
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 21:57:43 +0700

    Update kode720.py

--- Bab 7/kode720.py
+++ Bab 7/kode720.py
@@ -1,1 +1,11 @@
-aa
+#mencetak angka pada string berpola dengan %-formatting
+
+angka = 90 
+
+#cetak string berpola
+print("Ini angka %s" % angka)
+print("%s adalah string dari angka itu."% angka)
+print("%i adalah integer dari angka itu." % angka)
+print("%f adalah float dari angka itu." % angka)
+print("%o adalah octal dari angka itu." % angka)
+print("%x adalah octal dari angka itu." % angka)
