d19b90340fb0cfa586f516249b0b9772bea7daa6
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 19:23:02 +0700

    Create kode956.py

---
+++ Bab 9/kode956.py
@@ -0,0 +1,33 @@
+#menelusuri folder dan link simbolik
+
+import os
+import traceback as tb
+
+#tentukan direktori kerja
+dirKerja = "E:\TESTING"
+
+#telusuri folder" 
+try:
+    print("Posisi folder kerja: ", dirKerja)
+    #dpath = path direktori 
+    #dnames = nama2 direktori
+    #f = kumpulan file
+    #fnames = nama2 file
+    for dpath, dnames, f in os.walk(dirKerja,
+                                    followlinks = True):
+        if dnames == []:
+            print("")
+            print("Tidak ada subfolder di", 
+                  dpath, ", tapi ada file bernama:")
+            for fnames in f:
+                print(fnames)
+        else:
+            print("")
+            print("Menemukan subfolder", dnames, 
+                  "di", dpath, ", dan ada file di",
+                  dpath, "bernama: ")
+            for fname in f:
+                print(fnames)
+except BaseException as be:
+    be = tb.format_exc()
+    print("Error: ", be)
