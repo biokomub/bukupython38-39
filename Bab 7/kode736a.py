558ae7889410bd298ae25136938b5ab8903a8387
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 22:06:48 +0700

    Update and rename kode736.py to kode736a.py

---
+++ Bab 7/kode736a.py
@@ -0,0 +1,20 @@
+# string padding dengan rata kiri
+print("{:<11.2f}".format(62.85))
+
+# string padding dengan rata kiri
+# dan diapitkan dengan karakter "*"
+print("{:*<11.2f}".format(62.85))
+
+# string padding dengan rata kanan
+print("{:>11.2f}".format(62.85))
+
+# string padding dengan rata kanan
+# dan diapitkan dengan karakter "*"
+print("{:*>11.2f}".format(62.85))
+
+# string padding dengan rata tengah
+print("{:^11.2f}".format(62.85))
+
+# string padding dengan rata tengah
+# dan diapitkan dengan karakter "*"
+print("{:*^11.2f}".format(62.85))
