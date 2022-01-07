324a8d612d3cf523c5754e9b68a1b7c1c4e26b3c
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 22:04:04 +0700

    Update kode732.py

--- Bab 7/kode732.py
+++ Bab 7/kode732.py
@@ -1,1 +1,11 @@
-a
+# tampilkan tanda sesuai angka
+print("{:=f} {:=f}".format(45.23, -67.89))
+
+# tampilkan tanda + bila bertemu angka positif
+print("{:+f} {:+f}".format(45.23, -67.89))
+
+# tampilkan tanda - bila bertemu angka negatif
+print("{:-f} {:-f}".format(45.23, -67.89))
+
+# tampilkan spasi bila bertemu angka positif
+print("{: f} {: f}".format(45.23, -67.89))
