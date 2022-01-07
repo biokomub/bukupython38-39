5e9188038833ecc930b27e9899bd17a3443c8fc9
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 21:50:34 +0700

    Update kode74.py

--- Bab 7/kode74.py
+++ Bab 7/kode74.py
@@ -1,1 +1,33 @@
-a
+#contoh konversi tipe data
+
+a = 1                 # int
+b = 3.14              # float
+c = [1, 2, 3]         # list
+d = (1, 2, 3)         # tuple
+e = {"A" : 1, "B" :2} # dictionary
+f = None              # none
+
+a1 = str(a)
+b1 = str(b)
+c1 = str(c)
+d1 = str(d)
+e1 = str(e)
+f1 = str(f)
+
+print("Tipe data a sebelum : ", type(a))
+print("Tipe data a sesudah : ", type(a1))
+
+print("Tipe data b sebelum : ", type(b))
+print("Tipe data b sesudah : ", type(b1))
+
+print("Tipe data c sebelum : ", type(c))
+print("Tipe data c sesudah : ", type(c1))
+
+print("Tipe data d sebelum : ", type(d))
+print("Tipe data d sesudah : ", type(d1))
+
+print("Tipe data e sebelum : ", type(e))
+print("Tipe data e sesudah : ", type(e1))
+
+print("Tipe data f sebelum : ", type(f))
+print("Tipe data f sesudah : ", type(f1))
