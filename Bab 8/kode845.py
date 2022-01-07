9b7530019b2c19879218ea434b09c45da0114b40
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 18:36:40 +0700

    Create kode845.py

---
+++ Bab 8/kode845.py
@@ -0,0 +1,10 @@
+#membuat set
+contohfrzset = frozenset({11, 22, 33, 44, 55})
+frzset2list = list(contohset)
+
+#mengekstrak elemen set dari hasil konversi set -> list
+print(frzset2list[0])
+print(frzset2list[1])
+print(frzset2list[2])
+print(frzset2list[3])
+print(frzset2list[4])
