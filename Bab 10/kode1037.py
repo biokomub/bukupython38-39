d9ed4418354166573ec7ebc5c5fcaef3cfa00845
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 19:57:11 +0700

    Create kode1037.py

---
+++ Bab 10/kode1037.py
@@ -0,0 +1,7 @@
+def hubungkanSistem(user = "tamu", k_a):
+    print("Hubungkan sistem...")
+    print("Nama anda:", user) 
+    print("Kode akses:", k_a)
+    print("Sistem terhubung! Selamat datang", user)
+
+hubungkanSistem("asisten", "6")
