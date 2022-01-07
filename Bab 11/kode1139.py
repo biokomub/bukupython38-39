e64b771b684586526dbb97dc7ddd432ea3a4d36f
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 20:36:31 +0700

    Create kode1139.py

---
+++ Bab 11/kode1139.py
@@ -0,0 +1,9 @@
+class KelasA(object): 
+    def __init__(self): 
+        pass
+
+objA = KelasA()
+
+print("Tipe object:", (type(object)))
+print("Tipe KelasA:", (type(KelasA)))
+print("Tipe objA:", (type(objA)))
