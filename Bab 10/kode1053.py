0d83843a07ee1b18ec89d1e1fbc4301f32d2bb4b
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 20:13:05 +0700

    Create kode1053.py

---
+++ Bab 10/kode1053.py
@@ -0,0 +1,17 @@
+def fungsinama():
+    print("Halo, saya fungsi!")    
+
+def fungsinama(arg1):
+    print("Halo, saya", arg1)    
+    
+def fungsinama(arg1, arg2):
+    print("Halo, saya", arg1, "dan ini", arg2)    
+
+def fungsinama(num1, num2):
+    print("Halo, saya", num1, "dan ini", num2)    
+
+#panggil fungsi
+fungsinama()
+fungsinama("Viol")
+fungsinama("Samuel", "Putra")
+fungsinama(1, 3)
