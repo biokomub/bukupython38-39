f9a7e5591146cde95b93c6ea5dac5956764c6587
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 19:41:51 +0700

    Create kode1010.py

---
+++ Bab 10/kode1010.py
@@ -0,0 +1,13 @@
+# variable scope
+
+# ini variabel global
+var = 69
+
+#definisikan fungsi
+def fungsiScope():
+    var = 96
+    return var
+    
+print("Isi variabel di luar fungsi: ", var)
+print("Isi variabel di dalam fungsi: ", fungsiScope())
+print("Isi variabel di luar fungsi sekarang: ", var)
