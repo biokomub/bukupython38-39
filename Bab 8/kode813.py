c6fff9a1e4e048ecae9888adffa1cb2488055064
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-07 18:21:55 +0700

    Create kode813.py

---
+++ Bab 8/kode813.py
@@ -0,0 +1,6 @@
+#membuat list komposit
+judul_tabel = ["No", "Nama taksa", ["Genus", "Species"], "Asal"]
+record = ["2", "Melati Putih", ["Jasminum", "sambac"], "Malang"]
+
+print(judul_tabel[2][0])
+print(record[2][0])
