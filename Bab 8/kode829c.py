7eaf146444726eefed005f990102ef79f6a2d8e7
Laboratorium Biologi Komputasi dan Bioinformatika Jurusan Biologi FMIPA UB <94002828+biokomub@users.noreply.github.com>
2022-01-09 09:55:09 +0700

    Create kode829c.py

---
+++ Bab 8/kode829c.py
@@ -0,0 +1,7 @@
+# membuat dictionary
+buah = {"buah1": "apel", "buah2": "jeruk", "buah3": "nangka"}
+
+# mencetak isi dictionary
+print(buah.get("buah1"))
+print(buah.get("buah2"))
+print(buah.get("buah3"))
